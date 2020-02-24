7e12dc1c7c81c71e451ac490395325183d37743f
lowellhs <lowell.sianipar@gmail.com>
2020-02-24 14:39:45 +0700

    refactor

--- test_output/summary.sh
+++ test_output/summary.sh
@@ -5,7 +5,7 @@ for i in 0 1
 do
   for j in 0 1 2 3 4 5
   do
-    python3 total_time_extractor.py $env $i $j >> $out
+    python3 time_extractor.py $env $i $j >> $out
     echo "" >> $out
   done
   echo "----------------------------------------------" >> $out
