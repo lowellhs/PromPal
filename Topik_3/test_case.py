c2456797724e6f7655c6c1960d95d4580fa493e6
lowellhs <lowell.sianipar@gmail.com>
2020-02-26 11:16:51 +0700

    refactor

--- Topik_3/test_case.py
+++ Topik_3/test_case.py
@@ -17,7 +17,7 @@ x = np.dot(np.linalg.inv(A), b)
 
 outA = "matrix_A_%dx%d.txt" % (n, n)
 outb = "vector_b_%dx1.txt" % (n)
-outx = "matrix_x_%dx1.txt" % (n)
+outx = "vector_x_%dx1.txt" % (n)
 
 with open(outA, "w") as fA:
     print_matrix(A, fA)
