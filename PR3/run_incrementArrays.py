99cfa0ae342718f77c270af8e2da9c76477fb9d0
lowellhs <lowell.sianipar@gmail.com>
2020-05-05 02:14:38 +0700

    change

--- PR3/run_incrementArrays.py
+++ PR3/run_incrementArrays.py
@@ -6,6 +6,6 @@ import re
 for size in [2**20, 2**27]:
 	for blockSize in [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]:
 		nBlocks = size // blockSize
-		cmd = 'nvprof --log-file result/result_incArr_{0}_{1}_{2}.txt ./incrementArrays.o {0} {1} {2}'.format(size, nBlocks, blockSize).split(" ")
+		cmd = 'nvprof --log-file result/result_incArr_{0}_{1}_{2}.txt ./incrementArrays.o {0} {2}'.format(size, nBlocks, blockSize).split(" ")
 		res = subprocess.check_output(cmd)
 		print(" ".join([line.decode("utf-8") for line in res.splitlines()]))
