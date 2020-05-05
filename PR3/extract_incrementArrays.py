b218abbef916d7b31a0f366ceb41e3bc936816a0
lowellhs <lowell.sianipar@gmail.com>
2020-05-05 22:14:34 +0700

    refactored

--- PR3/extract_incrementArrays.py
+++ PR3/extract_incrementArrays.py
@@ -17,21 +17,42 @@ def toSeconds(timeStr):
       t *= 1E-3
     return t    
 
-res = []
-for filePath in os.listdir('./result'):
-  match = re.match('result\_'+name+'\_([0-9]+)\_([0-9]+)\_([0-9]+).txt', filePath)
-  if match:
-    with open("result/"+filePath, "r") as fObj:
-      textLine = fObj.readlines()
-    lineSelected = [line for line in textLine if "incrementArrayOnDevice" in line][0]
-    lineSelected = lineSelected.replace("GPU activities:", "")
-    splitted = re.sub("\s+", " ", lineSelected).split(" ")
-    time = ("%.6f" % toSeconds(splitted[2]))
-    size = int(match.group(1))
-    nBlocks = int(match.group(2))
-    blockSize = int(match.group(3))
-    res.append((size, nBlocks, blockSize, time))
-res.sort(key=lambda tup: (tup[0], tup[1]))
+if name == "incArr":
+  res = []
+  for filePath in os.listdir('./result'):
+    match = re.match('result\_'+name+'\_([0-9]+)\_([0-9]+)\_([0-9]+).txt', filePath)
+    if match:
+      with open("result/"+filePath, "r") as fObj:
+        textLine = fObj.readlines()
+      lineSelected = [line for line in textLine if "incrementArrayOnDevice" in line][0]
+      lineSelected = lineSelected.replace("GPU activities:", "")
+      splitted = re.sub("\s+", " ", lineSelected).split(" ")
+      time = ("%.6f" % toSeconds(splitted[2]))
+      size = int(match.group(1))
+      nBlocks = int(match.group(2))
+      blockSize = int(match.group(3))
+      res.append((size, nBlocks, blockSize, time))
+  res.sort(key=lambda tup: (tup[0], tup[1]))
+  for i in res:
+    print("%20s %20s %10s %10s" % i)
+elif name == "incArr2":
+  res = []
+  for filePath in os.listdir('./result'):
+    match = re.match('result\_'+name+'\_([0-9]+)\_([0-9]+)-([0-9]+)\_([0-9]+)-([0-9]+).txt', filePath)
+    if match:
+      with open("result/"+filePath, "r") as fObj:
+        textLine = fObj.readlines()
+      lineSelected = [line for line in textLine if "incrementArrayOnDevice" in line][0]
+      lineSelected = lineSelected.replace("GPU activities:", "")
+      splitted = re.sub("\s+", " ", lineSelected).split(" ")
+      time = ("%.6f" % toSeconds(splitted[2]))
+      size = int(match.group(1))
+      blockDimX = int(match.group(2))
+      blockDimY = int(match.group(3))
+      gridDimX = int(match.group(4))
+      gridDimY = int(match.group(5))
+      res.append((size, gridDimX, gridDimY, blockDimX, blockDimY, time))
+  res.sort(key=lambda tup: (tup[1], tup[2], tup[3], tup[4]))
+  for i in res:
+    print("%10s %10s %10s %10s" % (i[0], str((i[1],i[2])), str((i[3],i[4])), i[5]))
 
-for i in res:
-  print("%20s %20s %10s %10s" % i)
