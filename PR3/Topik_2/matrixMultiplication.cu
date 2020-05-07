55436a14e7d92d29b328f061f8dd72753b529e63
lowellhs <lowell.sianipar@gmail.com>
2020-05-07 19:46:53 +0000

    oke

--- PR3/Topik_2/matrixMultiplication.cu
+++ PR3/Topik_2/matrixMultiplication.cu
@@ -23,87 +23,75 @@ __global__ void matmulOnDevice(int n, float *A, float *B, float *C)
 
 int main(int argc, char **argv)
 {
-  struct timeval startCPU, stopCPU, startGPU, stopGPU;
-
-  float *A_h, *B_h, *C_h, *C2_h;  // pointers to host memory
-  float *A_d, *B_d, *C_d;         // pointers to device memory
-
-  // size of matrix (n*n)
-  int n = atoi(argv[1]);
-  size_t size = n*n*sizeof(float);
-
-  // allocate array on host
-  A_h  = (float *)malloc(size);
-  B_h  = (float *)malloc(size);
-  C_h  = (float *)malloc(size);
-  C2_h = (float *)malloc(size);
-
-  // allocate array on device
-  cudaMalloc((void **) &A_d, size);
-  cudaMalloc((void **) &B_d, size);
-  cudaMalloc((void **) &C_d, size);
-
-  // initializtion of host data
-  initIdentityMatrix(n, A_h);
-  initRandomMatrix(n, B_h);
-  
-  gettimeofday(&startGPU, 0);
-  // copy data from host to device
-  cudaMemcpy(A_d, A_h, n*n*sizeof(float), cudaMemcpyHostToDevice);
-  cudaMemcpy(B_d, B_h, n*n*sizeof(float), cudaMemcpyHostToDevice);
-
-  // do calculation on device
-  // Part 1 of 2. Compute execution configuration
-  dim3 gridDim, blockDim;
-  if (argc == 7)
+  int sizeCounter = atoi(argv[1]);
+  for (int counter=2; counter < 2+sizeCounter; counter++)
   {
-    gridDim = dim3(atoi(argv[3]), atoi(argv[4]));
-    blockDim = dim3(atoi(argv[5]), atoi(argv[6]));
-  }
-  else
-  {
-    if (n*n <= 1024)
+    struct timeval startGPU, stopGPU;
+
+    float *A_h, *B_h, *C_h, *C2_h;  // pointers to host memory
+    float *A_d, *B_d, *C_d;         // pointers to device memory
+
+    // size of matrix (n*n)
+    int n = atoi(argv[counter]);
+    size_t size = n*n*sizeof(float);
+
+    // allocate array on host
+    A_h  = (float *)malloc(size);
+    B_h  = (float *)malloc(size);
+    C_h  = (float *)malloc(size);
+    C2_h = (float *)malloc(size);
+
+    // allocate array on device
+    cudaMalloc((void **) &A_d, size);
+    cudaMalloc((void **) &B_d, size);
+    cudaMalloc((void **) &C_d, size);
+
+    // initializtion of host data
+    initIdentityMatrix(n, A_h);
+    initRandomMatrix(n, B_h);
+    
+    gettimeofday(&startGPU, 0);
+    // copy data from host to device
+    cudaMemcpy(A_d, A_h, n*n*sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(B_d, B_h, n*n*sizeof(float), cudaMemcpyHostToDevice);
+
+    // do calculation on device
+    // Part 1 of 2. Compute execution configuration
+    dim3 gridDim, blockDim;
+    if (argc == (2+sizeCounter+4))
     {
-      gridDim = dim3(1, 1);
-      blockDim = dim3(n, n);
+      gridDim = dim3(atoi(argv[argc-4]), atoi(argv[argc-3]));
+      blockDim = dim3(atoi(argv[argc-2]), atoi(argv[argc-1]));
     }
     else
     {
-      gridDim = dim3((int)ceil(n/32.0), (int)ceil(n/32.0));
-      blockDim = dim3(32, 32);
+      if (n*n <= 1024)
+      {
+        gridDim = dim3(1, 1);
+        blockDim = dim3(n, n);
+      }
+      else
+      {
+        gridDim = dim3((int)ceil(n/32.0), (int)ceil(n/32.0));
+        blockDim = dim3(32, 32);
+      }
     }
-  }
-  printf("gridDim: (%d,%d), blockDim: (%d,%d)\n", gridDim.x, gridDim.y, blockDim.x, blockDim.y);
-  
-  // Part 2 of 2. Call matmulOnDevice kernel
-  matmulOnDevice<<<gridDim, blockDim>>>(n, A_d, B_d, C_d);
-  cudaMemcpy(C2_h, C_d, n*n*sizeof(float), cudaMemcpyDeviceToHost);
+    
+    // Part 2 of 2. Call matmulOnDevice kernel
+    matmulOnDevice<<<gridDim, blockDim>>>(n, A_d, B_d, C_d);
+    cudaMemcpy(C2_h, C_d, n*n*sizeof(float), cudaMemcpyDeviceToHost);
 
-  cudaDeviceSynchronize();
-  gettimeofday(&stopGPU, 0);
+    cudaDeviceSynchronize();
+    gettimeofday(&stopGPU, 0);
 
-  // print matrix OR check matrix OR print time
-  if (atoi(argv[2]) == 0) printMatrix(n, C2_h);
-  if (atoi(argv[2]) == 1) // compare to CPU time and CPU result
-  {
-    // do calculation on host
-    gettimeofday(&startCPU, 0);
-    matmul(n, A_h, B_h, C_h);
-    float err = errorMatrix(n, C2_h, C_h);
-    gettimeofday(&stopCPU, 0);
-
-    printf("CPU time : %.6f\n", (stopCPU.tv_sec+stopCPU.tv_usec*1e-6)-(startCPU.tv_sec+startCPU.tv_usec*1e-6));
-    printf("error    : %.6f\n", err);
-  }
-  if (atoi(argv[2]) == 2) // if I.B = C means B should equals C (A is identity matrix)
-  {
     float err = errorMatrix(n, C2_h, B_h);
-    printf("error    : %.6f\n", err);
-  }
-  printf("GPU time : %.6f\n", (stopGPU.tv_sec+stopGPU.tv_usec*1e-6)-(startGPU.tv_sec+startGPU.tv_usec*1e-6));
+    printf("%d (%d,%d) (%d,%d) ", n, gridDim.x, gridDim.y, blockDim.x, blockDim.y);
+    printf("%.6f ", (stopGPU.tv_sec+stopGPU.tv_usec*1e-6)-(startGPU.tv_sec+startGPU.tv_usec*1e-6));
+    printf("%.6f\n", err);
 
-  // Cleanup
-  free(A_h); free(B_h); free(C_h); free(C2_h);
-  cudaFree(A_d); cudaFree(B_d); cudaFree(C_d);
+    // Cleanup
+    free(A_h); free(B_h); free(C_h); free(C2_h);
+    cudaFree(A_d); cudaFree(B_d); cudaFree(C_d);
+  }
 }
 
--- PR3/Topik_2/run_matrixMultiplication.py
+++ PR3/Topik_2/run_matrixMultiplication.py
@@ -7,18 +7,13 @@ def findingOptimalGridBlock():
   for sizePow in sizesPow:
     size = 2**sizePow
     gridsDimPow = [sizePow-i for i in blocksDimPow]
-    print("\n{0} * {0}".format(size))
-    arr.append("{0} * {0}".format(size))
     for counter in range(5):
       blockDim = 2**blocksDimPow[counter]
       gridDim = 2**gridsDimPow[counter]
-      cmd = "./matrixMultiplication.o {0} 2 {1} {1} {2} {2}".format(size, gridDim, blockDim)
+      cmd = "./matrixMultiplication.o 1 {0} {1} {1} {2} {2}".format(size, gridDim, blockDim)
       res = subprocess.check_output(cmd.split(" "))
-      lines = [line.decode("utf-8") for line in res.splitlines()]
-      arr.append("{0:<40s} {1} {2}".format(lines[0], lines[1], lines[2]))
+      arr.append(res.decode("utf-8").strip())
       print(arr[-1])
-    arr.append("")
-    print(arr[-1])
   with open("result/result_varGridBlock.txt", "w") as f:
     for ar in arr:
       print(ar, file=f)
@@ -37,5 +32,5 @@ def compareCompTime():
         print("\n".join(lines), file=fobj)
         print("", file=fobj)
 
-# findingOptimalGridBlock()
-compareCompTime()
+findingOptimalGridBlock()
+# compareCompTime()
