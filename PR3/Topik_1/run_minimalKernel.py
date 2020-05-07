3f552b8df33d5bbaa6b0fbee799c89334856627d
Rasyid Gatrawijaya <rasyid.gatrawijaya@gmail.com>
2020-05-07 14:22:21 +0700

    [REFACTOR] fix py for file outputs

--- PR3/Topik_1/run_minimalKernel.py
+++ PR3/Topik_1/run_minimalKernel.py
@@ -11,13 +11,20 @@ r  = [
         (20,10,2),
         (20,20,1)
 ]
+
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+MINIMALKERNEL_RESULT_PATH = os.path.join(THIS_FOLDER, 'result/minimalKernel/')
+if not os.path.exists(MINIMALKERNEL_RESULT_PATH):
+    os.makedirs(MINIMALKERNEL_RESULT_PATH)
+
 for item in r:
-    n, block, thread = item
-    cmd = './minimalKernel.o {0} {1} {2}'.format(n, block, thread).split(" ")
-    res = subprocess.check_output(cmd)
-    #orig_sys = sys.stdout
-    print('<<<{0}, {1}>>>'.format(block, thread))
-    print(res.decode('utf-8'))
-    print()
-    #file = open('result/minimalKernel/result_minimalKernel_{0}_{1}.txt'.format(block, thread), 'w') 
-    #file.write(res)
+        n, block, thread = item
+        cmd = './minimalKernel.o {0} {1} {2}'.format(n, block, thread).split(" ")
+        res = subprocess.check_output(cmd)
+        result_file = os.path.join(MINIMALKERNEL_RESULT_PATH, 'result_minimalKernel_{0}_{1}.txt'.format(block, thread))
+        file = open(result_file, 'w') 
+        file.write(res)
+        file.close()
+        print('<<<{0}, {1}>>>'.format(block, thread))
+        print(res.decode('utf-8'))
+        print()
