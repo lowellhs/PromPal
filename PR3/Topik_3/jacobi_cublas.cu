181434159702891e15158056da909d6011d6f0d7
Rasyid Gatrawijaya <rasyid.gatrawijaya@gmail.com>
2020-05-17 00:27:47 +0000

    Fix cuBLAS and update test inputs

--- PR3/Topik_3/jacobi_cublas.cu
+++ PR3/Topik_3/jacobi_cublas.cu
@@ -6,7 +6,7 @@
 #include "cublas_v2.h"
 
 #define TOL 1e-6
-#define limit_iter 31
+#define limit_iter 10000
 
 __device__ int flag;
 
@@ -33,7 +33,6 @@ __global__ void checkConvergence(int n, float *x_iter, float *x_iter_new)
   if (i<n)
   {
     if (fabs(x_iter[i]-x_iter_new[i]) > TOL) flag = 0;
-		printf("%.6f %.6f\n", x_iter[i], x_iter_new[i]);
     x_iter[i] = x_iter_new[i];
   }
 }
@@ -48,8 +47,7 @@ int main(int argc, char **argv)
     {
       cudaEvent_t start, stop;
 
-      float *A, *b, *x, *x_iter, *x_iter_new, *D_inv, *LU;
-      float *x_lu_result, *x_bminx_result;
+      float *A, *b, *x, *x_iter, *x_iter_new, *D_inv, *LU, *T, *c;
       float al, bet;
 
       int n = atoi(argv[counter]);
@@ -60,8 +58,8 @@ int main(int argc, char **argv)
       cudaMallocManaged(&x_iter_new, n*sizeof(float));
       cudaMallocManaged(&D_inv, n*n*sizeof(float));
       cudaMallocManaged(&LU, n*n*sizeof(float));
-      cudaMallocManaged(&x_lu_result, n*sizeof(float));
-      cudaMallocManaged(&x_bminx_result, n*sizeof(float));
+      cudaMallocManaged(&T, n*n*sizeof(float));
+      cudaMallocManaged(&c, n*sizeof(float));
       
       char file_A[80], file_b[80], file_x[80];
       sprintf(file_A, "test_input/matrix_A_%dx%d.txt", n, n);
@@ -86,38 +84,38 @@ int main(int argc, char **argv)
 
       cublasHandle_t handle;
       cublasCreate(&handle);
+      al = 1.0f, bet = 0.0f;
+      cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, n, n, n, &al, LU, n, D_inv, n, &bet, T, n);
+      cublasSgemv(handle, CUBLAS_OP_N, n, n, &al, D_inv, n, b, 1, &bet, c, 1);
+      cudaDeviceSynchronize();
       int k = 0, isConverged;
       do {
-        isConverged = 0;
+        isConverged = 1;
+
         cudaMemcpyToSymbol(flag, &isConverged, sizeof(int));
-
-        al = 1.0f;
-        bet = 0.0f;
-        // printVector(n, x_iter); printf("\n");
-        cublasSgemv(handle, CUBLAS_OP_N, n, n, &al, LU, n, x_iter, 1, &bet, x_lu_result, 1);
-        al = 1.0f; cublasSaxpy(handle, n, &al, b, 1, x_lu_result, 1);
-        al = 1.0f; cublasSgemv(handle, CUBLAS_OP_N, n, n, &al, D_inv, n, x_lu_result, 1, &bet, x_iter_new, 1);
+        cublasSgemv(handle, CUBLAS_OP_T, n, n, &al, T, n, x_iter, 1, &bet, x_iter_new, 1);
+        cublasSaxpy(handle, n, &al, c, 1, x_iter_new, 1);
+        checkConvergence<<<(int)ceil(n/1024.0), 1024>>>(n, x_iter, x_iter_new);
         cublasScopy(handle, n, x_iter_new, 1, x_iter, 1);
-
-        cudaDeviceSynchronize();
-        printVector(n, x_iter); printf("\n");
         cudaMemcpyFromSymbol(&isConverged, flag, sizeof(int));
+        cudaDeviceSynchronize();
+
         k++;
       } while (k < limit_iter && !isConverged);
       cublasDestroy(handle);
 
-      // cudaEventRecord(stop);
-      // cudaEventSynchronize(stop);
-      // float milliseconds = 0; cudaEventElapsedTime(&milliseconds, start, stop);
-      // cudaDeviceSynchronize();
+      cudaEventRecord(stop);
+      cudaEventSynchronize(stop);
+      float milliseconds = 0; cudaEventElapsedTime(&milliseconds, start, stop);
+      cudaDeviceSynchronize();
 
       printf("%d ", n);
-      // printf("%.6f ", milliseconds*1e-3);
+      printf("%.6f ", milliseconds*1e-3);
       printf("%.9f ", norm_vector(n, x_iter, x));
       printf("%d\n", k);
 
       cudaFree(A); cudaFree(b); cudaFree(x); cudaFree(x_iter); cudaFree(x_iter_new); cudaFree(D_inv); cudaFree(LU);
-      cudaFree(x_lu_result); cudaFree(x_bminx_result);
+      cudaFree(T); cudaFree(c);
     }
   }
   return 0;
